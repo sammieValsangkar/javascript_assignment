72912619e3cb81b869af0e8de28681dabe8899d1
sammyValsangkar <sameer@cart91.com>
2017-03-10 11:03:55 +0530

    color added in settings

--- assignment_4/js/custom.js
+++ assignment_4/js/custom.js
@@ -4,7 +4,12 @@ window.onload = function () {
 
     settings: {
       switchState : "OFF",
-      switcher : document.getElementById('switcher')
+      switcher : document.getElementById('switcher'),
+      backgroundColor1 : "red",
+      backgroundColor2 : "yellow",
+      rowColor1 : "black",
+      rowColor2 : "grey",
+      buttonColor1 : "green"
     },
 
     init: function() {
@@ -29,16 +34,16 @@ window.onload = function () {
     changeState: function(switchState) {
       if( switchState =='OFF') {
         SwitchControl.selectById('switch-status').innerHTML = 'ON';
-        SwitchControl.setBackground(SwitchControl.selectById('switch-container'),"yellow");
+        SwitchControl.setBackground(SwitchControl.selectById('switch-container'),s.backgroundColor2);
         SwitchControl.showMessage();
-        SwitchControl.alternator('black','grey');
+        SwitchControl.alternator(s.rowColor1,s.rowColor2);
         return 'ON';
       }
       else {
         SwitchControl.selectById('switch-status').innerHTML = 'OFF';
-        SwitchControl.setBackground(SwitchControl.selectById('switch-container'),"red");
+        SwitchControl.setBackground(SwitchControl.selectById('switch-container'),s.backgroundColor1);
         SwitchControl.hideMessage();
-        SwitchControl.alternator('grey','black');
+        SwitchControl.alternator(s.rowColor2,s.rowColor1);
         return 'OFF';
       }
     },
@@ -68,7 +73,7 @@ window.onload = function () {
       switcher = SwitchControl.selectById('switcher');
       if(SwitchControl.selectById('capture').checked)
       {
-        SwitchControl.setBackground(SwitchControl.selectById('switcher'),"green");
+        SwitchControl.setBackground(SwitchControl.selectById('switcher'),s.buttonColor1);
       }
       else
       {
